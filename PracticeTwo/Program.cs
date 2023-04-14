3ebb1c95a529d625d87c5ed5727028826dfcb9a5
Mauricio Terceros <mauricio.terceros@bo.truextend.com>
2023-04-13 20:37:44 -0400

    Created new project and configure Swagger.

---
+++ PracticeTwo/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.OpenApi.Models;
+
+// This link for serilog
+// https://rmauro.dev/setup-serilog-in-net6-as-logging-provider/
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+var configurationBuilder = new ConfigurationBuilder()
+        .SetBasePath(builder.Environment.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+IConfiguration Configuration = configurationBuilder.Build();
+string siteTitle = Configuration.GetSection("Title").Value;
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = siteTitle
+    });
+});
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline. FOR LATER
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
